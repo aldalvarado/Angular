47161cfbd5b189480450c5e1794c73616019cd11
aldalvarado <aaldoandre@gmail.com>
2024-01-17 17:47:39 -0600

    Tarea

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -4,16 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-heroes-hero',
   standalone: true,
   templateUrl: "./hero.component.html",
-  styleUrl: "./hero.component.css"
+  styleUrl: "./hero.component.css",
 })
 
 export class HeroComponent {
   public name: string = "Iron Man";
   public age: number = 45;
+
   get CapitalizedName(): string {
     return this.name.toUpperCase();
   }
+
   getHeroDescription(): string {
     return `${this.name} - ${this.age}`;
   }
+
+  ChangeName():void{
+    this.name = "Spider Man";
+  }
+
+  ChangeAge():void{
+    this.age = 23;
+  }
+
 }
