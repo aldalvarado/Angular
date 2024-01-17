c2e8e9dcffba35c217e981e0f5587147f7f24d5a
aldalvarado <aaldoandre@gmail.com>
2024-01-17 16:21:42 -0600

    Heroes and List

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  standalone: true,
+  template: "Hero Works"
+})
+
+export class HeroComponent {
+
+}
---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  standalone: true,
+  template: "List Works",
+})
+
+export class ListComponent {
+}
