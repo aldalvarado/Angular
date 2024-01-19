7306f85cce37d99770f280f4ec6bfd14778cf885
aldalvarado <aaldoandre@gmail.com>
2024-01-18 18:43:56 -0600

    NgIF

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-heroes-list',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
