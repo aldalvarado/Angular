6544ad1852c470e05a3a7a0dbfaa9f1130efdf79
aldalvarado <aaldoandre@gmail.com>
2024-01-26 21:14:40 -0600

    Servicios Privados

--- src/app/DBZ/pages/main-page.component.ts
+++ src/app/DBZ/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DBZService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -8,6 +9,16 @@ import { DBZService } from '../services/dbz.service';
 })
 
 export class MainPageComponent {
-  constructor(public DbzService: DBZService) {
+  constructor(private DbzService: DBZService) {}
+  get Characters(): Character[] {
+    return [...this.DbzService.Characters]
+  }
+
+  OnDeleteCharacter(id: string): void {
+    this.DbzService.DeleteCharacterByID(id)
+  }
+
+  OnNewCharacter(Characters: Character): void {
+    this.DbzService.AddCharacter(Characters);
   }
 }
